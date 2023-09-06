71777b019decc38c4a035fee36b6c64d54d0d063
Ivan <ivan8830@yandex.ru>
2023-09-06 09:54:09 +0500

    немного оптимизации

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import {inc, dec, rnd} from './action';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(reducer); //это хранилеще
+
+const {dispatch, subscribe, getState} = store;
+
+const update = () => {
+    document.getElementById('counter').textContent = getState().value;
+}
+
+subscribe(update);
+
+const incDispatch = () => dispatch(inc());
+const decDispatch = () => dispatch(dec());
+const rndDispatch = (value) => dispatch(rnd(value));
+
+const inc = () => {
+    return {
+        type: 'INC'
+    }
+}
+
+const dec = () => {
+    return {
+        type: 'DEC'
+    }
+}
+
+const rnd = (value) => {
+    return {
+        type: 'RND',
+        payload: value
+    }
+}
+
+document.getElementById('inc').addEventListener('click', incDispatch);
+
+document.getElementById('dec').addEventListener('click', decDispatch)
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    rndDispatch(value)
+})
+
+
+
+
+
+let state = reducer(initialState, {type: 'INC'});
+console.log(state);
+
+ReactDOM.render(
+    <React.StrictMode>
+        <>
+      
+        </>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
