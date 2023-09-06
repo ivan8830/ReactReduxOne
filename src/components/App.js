fd96086af35bc137d7e24d646cc49425b7eb9a26
Ivan <ivan8830@yandex.ru>
2023-09-06 11:09:22 +0500

    Использование Provider для переброски данных

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import Counter from "./Counter";
+import * as actions from './action';
+
+const App = () => {
+    return <Counter/>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import * as actions from './action';
 
-import Counter from './Counter';
+import {Provider} from 'react-redux';
+
+import App from './components/App';
 
 const store = createStore(reducer); //это хранилеще
 
-const {dispatch, subscribe, getState} = store;
-const {inc, dec, rnd} = bindActionCreators( actions, dispatch);
-
-const update = () => {
     ReactDOM.render(
         <React.StrictMode>
             <>
-                <Counter
-                    counter={getState().value}
-                    inc={inc}
-                    dec={dec}
-                    rnd={() => {
-                        const value = Math.floor(Math.random() * 10);
-                        rnd(value)
-                    }}
-                />
+                <Provider store={store}>
+                    <App/> 
+                </Provider>
+
             </>
         </React.StrictMode>,
         document.getElementById('root')
     );
-}
-update();
-subscribe(update);
 
 /* const bindActionCreator = (creator, dispatch) => (...args) => {//функция bindActionCreator возвращает новую функция которая будет что то возвращать
     dispatch(creator(...args));
